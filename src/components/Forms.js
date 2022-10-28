372d50d29fbfb177adbce1c38fe2c2a7dd06d68f
matvey733 <morozovmatvej75@gmail.com>
2022-10-28 19:56:51 +0300

    Type of school & data display

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
 export default function Forms() {
+    
     const [data, setData] = useState(
         {
-            headsOrTails: "",
+            typeOfSchool: "",
             termsOfServiceAgree: false
         }
     )
-    
+    console.log(data);
+        
     function handleChange(event) {
         const { type, name, value, checked } = event.target
 
         setData(prevState => {
-            console.log(prevState);
             return {
                 ...prevState,
                 [name]: type === "checkbox" ? checked : value
@@ -20,20 +21,53 @@ export default function Forms() {
         })
     }
 
+
     return (
         <div className="forms">
-            <div className="input-wrapper">
-                <input 
-                    type="checkbox"
-                    checked={data.termsOfServiceAgree}
-                    name="termsOfServiceAgree"
-                    id="termsOfServiceAgree"
-                    onChange={handleChange}
-                />
-                <label htmlFor="termsOfServiceAgree">I agree with the terms of service</label>
-            </div>    
+            <input 
+                type="checkbox"
+                checked={data.termsOfServiceAgree}
+                name="termsOfServiceAgree"
+                id="termsOfServiceAgree"
+                onChange={handleChange}
+            />
+            <label htmlFor="termsOfServiceAgree">I agree with the terms of service</label>
 
-            {data.termsOfServiceAgree ? "Agreed to the terms of service" : "Disagreed with the terms of service"}
+            <fieldset className="field-set">
+                <legend>Type of school you attend:</legend>
+
+                <div className="input-wrapper">
+                    <input 
+                        type="radio"
+                        name="typeOfSchool"
+                        id="option1"
+                        onChange={handleChange}
+                        value="private"
+                    />
+                    <label htmlFor="option1">Private</label>
+                </div>
+
+                <div className="input-wrapper">
+                        <input 
+                        type="radio"
+                        name="typeOfSchool"
+                        id="option2"
+                        onChange={handleChange}
+                        value="public"
+                    />
+                    <label htmlFor="option2">Public</label>
+                </div>
+            </fieldset>
+                
+            <div className="data-display">
+
+                <h1>Data entered:</h1>
+                <ol>
+                    <li>{data.termsOfServiceAgree ? "Agreed to the terms of service" : "Disagreed with the terms of service"}</li>
+                    {data.typeOfSchool && <li>School type: {data.typeOfSchool}</li>}
+                </ol>
+                
+            </div>
         </div>
     )
 }
