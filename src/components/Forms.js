66bd9bd2409e7b0880e8b7726b4fe1ff9329e32a
Matvey <morozovmatvej75@gmail.com>
2022-10-31 10:30:28 +0300

    Indentation

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Forms(props) {        
     function handleChange(event) {
@@ -47,134 +47,133 @@ export default function Forms(props) {
     }
 
     return (
-        <form className="form" onSubmit={handleSubmit}>
-            <h1 className="form-header">Form:</h1>
-            
-            <div className="question">
-                <div className="input-wrapper">
-                    <label htmlFor="firstName">First name:</label>
+        <>
+            <form className="form" onSubmit={handleSubmit}>
+                <h1 className="form-header">Form:</h1>
+                
+                <div className="question">
+                    <div className="input-wrapper">
+                        <label htmlFor="firstName">First name:</label>
+                        <input 
+                            type="text"
+                            id="firstName"
+                            className="input"
+                            name="firstName"
+                            onChange={handleChange}
+                            value={props.data.data.firstName}
+                        />
+                    </div>
+                </div>
+
+                <div className="question">
+                    <label htmlFor="lastName">Last name:</label>
                     <input 
                         type="text"
-                        id="firstName"
+                        id="lastName"
                         className="input"
-                        name="firstName"
+                        name="lastName"
                         onChange={handleChange}
-                        value={props.data.data.firstName}
+                        value={props.data.data.lastName}
                     />
                 </div>
-            </div>
 
-            <div className="question">
-                <label htmlFor="lastName">Last name:</label>
-                <input 
-                    type="text"
-                    id="lastName"
-                    className="input"
-                    name="lastName"
-                    onChange={handleChange}
-                    value={props.data.data.lastName}
-                />
-            </div>
+                <div className="question">
+                    <label htmlFor="age">Age:</label>
+                    <input 
+                        type="number"
+                        className="input"
+                        name="age"
+                        id="age"
+                        onChange={handleChange}
+                        value={props.data.data.age}
+                        min="0"
+                        max="27"
+                    />
+                </div>
 
-            <div className="question">
-                <label htmlFor="age">Age:</label>
-                <input 
-                    type="number"
-                    className="input"
-                    name="age"
-                    id="age"
-                    onChange={handleChange}
-                    value={props.data.data.age}
-                    min="0"
-                    max="27"
-                />
-            </div>
+                <div className="question">
+                    <input 
+                        type="radio"
+                        className="input"
+                        name="gender"
+                        id="gender-option1"
+                        value="male"
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="gender-option1">Male</label>
+                    <input 
+                        type="radio"
+                        className="input"
+                        name="gender"
+                        id="gender-option2"
+                        value="female"
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="gender-option2">Female</label>
+                </div>
 
-            <div className="question">
-                <input 
-                    type="radio"
-                    className="input"
-                    name="gender"
-                    id="gender-option1"
-                    value="male"
-                    onChange={handleChange}
-                />
-                <label htmlFor="gender-option1">Male</label>
-                <input 
-                    type="radio"
-                    className="input"
-                    name="gender"
-                    id="gender-option2"
-                    value="female"
-                    onChange={handleChange}
-                />
-                <label htmlFor="gender-option2">Female</label>
-            </div>
+                <div className="question">
+                    <fieldset className="field-set">
+                        <legend>Type of school you attend:</legend>
 
-            <div className="question">
-                <fieldset className="field-set">
-                    <legend>Type of school you attend:</legend>
-
-                    <div className="input-wrapper">
-                        <input 
-                            type="radio"
-                            name="typeOfSchool"
-                            id="school-option1"
-                            onChange={handleChange}
-                            value="private"
-                        />
-                        <label htmlFor="school-option1">Private</label>
-                    </div>
-
-                    <div className="input-wrapper">
+                        <div className="input-wrapper">
                             <input 
-                            type="radio"
-                            name="typeOfSchool"
-                            id="school-option2"
-                            onChange={handleChange}
-                            value="public"
-                        />
-                        <label htmlFor="school-option2">Public</label>
-                    </div>
-                </fieldset>
-            </div>
+                                type="radio"
+                                name="typeOfSchool"
+                                id="school-option1"
+                                onChange={handleChange}
+                                value="private"
+                            />
+                            <label htmlFor="school-option1">Private</label>
+                        </div>
 
-            <div className="question">
-                <label htmlFor="favColor">Your favorite color:</label>
-                <select
-                    className="select"
-                    id="favColor"
-                    name="favColor"
-                    value={props.data.data.favColor}
-                    onChange={handleChange}
-                >
-                    <option value="red">Red</option>
-                    <option value="blue">Blue</option>
-                    <option value="darkblue">Darkblue</option>
-                    <option value="green">Green</option>
-                    <option value="black">Black</option>
-                </select>
-            </div>
-            
-            <div className="question">
-                <input 
-                    type="checkbox"
-                    checked={props.data.data.termsOfServiceAgree}
-                    value={props.data.data.termsOfServiceAgree}
-                    name="termsOfServiceAgree"
-                    id="termsOfServiceAgree"
-                    onChange={handleChange}
-                />
-                <label htmlFor="termsOfServiceAgree">I agree with the terms of service</label>
+                        <div className="input-wrapper">
+                                <input 
+                                type="radio"
+                                name="typeOfSchool"
+                                id="school-option2"
+                                onChange={handleChange}
+                                value="public"
+                            />
+                            <label htmlFor="school-option2">Public</label>
+                        </div>
+                    </fieldset>
+                </div>
 
-                <a href="https://youtu.be/gokdrC4gQA4">
-                    <button className="secret-btn"></button>
-                </a>
-            </div>
+                <div className="question">
+                    <label htmlFor="favColor">Your favorite color:</label>
+                    <select
+                        className="select"
+                        id="favColor"
+                        name="favColor"
+                        value={props.data.data.favColor}
+                        onChange={handleChange}
+                    >
+                        <option value="red">Red</option>
+                        <option value="blue">Blue</option>
+                        <option value="darkblue">Darkblue</option>
+                        <option value="green">Green</option>
+                        <option value="black">Black</option>
+                    </select>
+                </div>
+                
+                <div className="question">
+                    <input 
+                        type="checkbox"
+                        checked={props.data.data.termsOfServiceAgree}
+                        value={props.data.data.termsOfServiceAgree}
+                        name="termsOfServiceAgree"
+                        id="termsOfServiceAgree"
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="termsOfServiceAgree">I agree with the terms of service</label>
 
-            <button className="submit-btn">Submit</button>
-
-        </form>
-        
+                    <a href="https://youtu.be/gokdrC4gQA4">
+                        <button className="secret-btn"></button>
+                    </a>
+                </div>
+                <button className="submit-btn">Submit</button>
+            </form>
+        </>        
     )
 }
